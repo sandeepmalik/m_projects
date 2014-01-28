c46eb800153ccbb25e755b098fc6542c77668f7b
smalik3 <smalik@walmartlabs.com>
2014-01-28 01:10:25 -0800

    Adding get API

--- web_service/src/main/java/com/personal/OfferService.java
+++ web_service/src/main/java/com/personal/OfferService.java
@@ -3,7 +3,9 @@ package com.personal;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -15,7 +17,7 @@ import java.util.Set;
 @Path("/offer")
 public class OfferService {
 
-    private static final Set<Offer> offers = new HashSet<Offer>();
+    private static final Map<String, Offer> offers = new HashMap<String, Offer>();
 
     @POST
     @Path("/create")
@@ -23,12 +25,21 @@ public class OfferService {
         if (offer.getStartDate() != null) {
             if (offer.getEndDate() == null) {
                 return Response.status(400).entity(new Error("endDate is needed if startDate is present.")).build();
-            } else if(offer.getEndDate().before(offer.getStartDate())) {
+            } else if (offer.getEndDate().before(offer.getStartDate())) {
                 return Response.status(400).entity(new Error("endDate must be greater than startDate.")).build();
             }
         }
         offer.setId(String.valueOf(System.currentTimeMillis()));
-        offers.add(offer);
+        offers.put(offer.getId(), offer);
         return Response.ok(offer).build();
+    }
+
+    @GET
+    public Response get(@QueryParam("id") String id) {
+        if (!offers.containsKey(id)) {
+            return Response.status(404).entity(new Error("Not Found")).build();
+        } else {
+            return Response.ok(offers.get(id)).build();
+        }
     }
 }
